944bd6205a72e9309e8cfaf30baaef23d98475ae
Jakebas10 <jake.bas10@mac.com>
2020-03-22 20:21:28 -0500

    finished site header next step is to add nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,13 @@ function Header() {
                 <div className="row">
                     <div className="col-md-1"/>
                     <img id="header-img" className="col-md-3 col-xs-4 vert-align" src="src/resources/headshot.jpg"/>
+                    <div id="header-info-outer-container" className="col-md-3 col-xs-4 vert-align">
+                        <h1>Jake Basten</h1>
+                        <div>
+                            <h3 className="header-title text-muted">Software Engineer</h3>
+                            <h4 className="text-muted">📍Chicago, IL</h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Fragment>
