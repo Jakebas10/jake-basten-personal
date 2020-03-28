d29515a0281f6c7ca0699ebb91a954afb959e964
Jake Basten and Mike Orozco <pair+jbasten+morozco@stats.com>
2020-03-28 15:38:56 -0500

    improved look of the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,22 @@
-import React, {Component, Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
 import '../css/menu.css';
 
 class Menu extends Component {
 
     render() {
-        
+
         const menuItems = ['About Me', 'Blog', 'Resume'];
-        return(
+        return (
             <Fragment>
-                <ul>
-                    {menuItems.map(item => (
-                        <li>{item}</li>
-                    ))}
-                </ul>
+                <div className="menu-container">
+                    <ul className="nav nav-tabs nav-fill">
+                        {menuItems.map(item => (
+                            <li className="nav-item">
+                                <a className="nav-link menu-item" href="#">{item}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </Fragment>
         );
     }
