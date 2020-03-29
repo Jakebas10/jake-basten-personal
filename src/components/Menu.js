ba565d732629a337b409bb6e23d6162b0c0a2b0e
Jake Basten and Mike Orozco <pair+jbasten+morozco@stats.com>
2020-03-29 17:15:29 -0500

    added functionality to change active tab visualization when clicked

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,18 +3,40 @@ import '../css/menu.css';
 
 class Menu extends Component {
 
-    render() {
+    constructor(props) {
+        super(props);
 
-        const menuItems = ['About Me', 'Blog', 'Resume'];
+        let menuItems = [
+            { name: 'About Me', active: 'active' },
+            { name: 'Blog', active: '' },
+            { name: 'Resume', active: '' }
+        ]
+
+        this.state = {
+            menuItems: menuItems
+        };
+    }
+
+    changeActive(name) {
+        let newItems = this.state.menuItems;
+        newItems.forEach(item =>
+            item.active = (name === item.name) ? 'active' : ''
+        );
+        this.setState({ menuItems: newItems });
+    }
+
+    render() {
         return (
             <Fragment>
                 <div className="menu-container">
                     <ul className="nav nav-tabs nav-fill">
-                        {menuItems.map(item => (
+                        {this.state.menuItems.map(item =>
                             <li className="nav-item">
-                                <a className="nav-link menu-item" href="#">{item}</a>
+                                <div className={"nav-link menu-item " + item.active} onClick={this.changeActive.bind(this, item.name)}>
+                                    {item.name}
+                                </div>
                             </li>
-                        ))}
+                        )}
                     </ul>
                 </div>
             </Fragment>
